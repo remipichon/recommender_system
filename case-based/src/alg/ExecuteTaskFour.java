42086abf61c8bf53c569250eba9537521e537e10
remipichon <pichon.remi.pr@gmail.com>
2016-02-10 10:02:28 +0000

    no message

--- case-based/src/alg/ExecuteTaskFour.java
+++ case-based/src/alg/ExecuteTaskFour.java
@@ -11,6 +11,7 @@ import alg.cases.similarity.CaseSimilarity;
 import alg.cases.similarity.OverlapCaseSimilarity;
 import alg.recommender.MaxRecommenderPersonalised;
 import alg.recommender.Recommender;
+import util.evaluator.Evaluator;
 import util.reader.DatasetReader;
 
 import java.io.File;
@@ -29,6 +30,16 @@ public class ExecuteTaskFour extends Execute{
 
 
         recommender = new MaxRecommenderPersonalised(overlapCaseSimilarity, reader);
-        evaluateAndPrintResult("overlapCaseSimilarity | MaxRecommender", reader, recommender);
+        evaluateAndPrintResultForDiversity("overlapCaseSimilarity | MaxRecommender", reader, recommender);
+    }
+
+    static void evaluateAndPrintResultForDiversity(String type, DatasetReader reader, Recommender recommender) {
+        // evaluate the case-based recommender
+        Evaluator eval = new Evaluator(recommender, reader);
+
+        System.out.println(type);
+        System.out.println("topN\tRecall\tPrecision for " + type);
+        for (int topN = 5; topN <= 50; topN += 5) //the size of the recommendation list
+            System.out.println(topN + "\t" + eval.getRecall(topN) + "\t" + eval.getPrecision(topN));
     }
 }
